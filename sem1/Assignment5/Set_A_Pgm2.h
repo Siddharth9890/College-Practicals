e80348bc90f0d9162082a1c69729a76353b27fd3
Siddharth9890 <63333707+Siddharth9890@users.noreply.github.com>
2021-02-28 15:20:32 +0530

    Update Set_A_Pgm2.h

--- sem1/Assignment5/Set_A_Pgm2.h
+++ sem1/Assignment5/Set_A_Pgm2.h
@@ -27,17 +27,24 @@ void create(node *head)
 
 void reverse(node *head)
 {
-    struct node *current, *temp;
-
-    current = head;
-    while(current != NULL)
+    struct node *t1=head->next,*t2,*t3;
+    // t1->prev=head;
+    if(t1==NULL)
+        return;
+    t2=t1->next;
+    if(t2==NULL)
+        return;
+    t3=t2->next;
+    t1->next=NULL;
+    while(t3!=NULL)
     {
-        temp = current->next;
-        current->next = current->prev;
-        current->prev = temp;
-        current = temp;
+        t2->next=t1;
+        t1=t2;
+        t2=t3;
+        t3=t3->next;
     }
-    
+    t2->next=t1;
+    head->next=t2;    
 }
 
 void display(node *head)
